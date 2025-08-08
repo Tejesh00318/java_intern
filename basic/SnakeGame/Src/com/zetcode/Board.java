74f103c5e9d1117e963a9958cfaf489ddd3a7381
Tejesh00318 <tejeshg92@gmail.com>
2025-08-08 16:18:39 +0530

    Add files via upload

--- basic/SnakeGame/Src/com/zetcode/Board.java
+++ basic/SnakeGame/Src/com/zetcode/Board.java
@@ -1,1 +1,204 @@
+package zetcode;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Board extends JPanel implements ActionListener {
+
+    private final int B_WIDTH = 300;
+    private final int B_HEIGHT = 300;
+    private final int DOT_SIZE = 10;
+    private final int ALL_DOTS = 900;
+    private final int RAND_POS = 29;
+    private final int DELAY = 140;
+
+    private final int[] x = new int[ALL_DOTS];
+    private final int[] y = new int[ALL_DOTS];
+
+    private int dots;
+    private int apple_x;
+    private int apple_y;
+    private int score;
+
+    private boolean leftDirection = false;
+    private boolean rightDirection = true;
+    private boolean upDirection = false;
+    private boolean downDirection = false;
+    private boolean inGame = true;
+
+    private Timer timer;
+    private Image ball;
+    private Image apple;
+    private Image head;
+
+    public Board() {
+        initBoard();
+    }
+
+    private void initBoard() {
+        addKeyListener(new TAdapter());
+        setBackground(Color.black);
+        setFocusable(true);
+        setPreferredSize(new Dimension(B_WIDTH, B_HEIGHT));
+        loadImages();
+        initGame();
+    }
+
+    private void loadImages() {
+        try {
+            ball = new ImageIcon("src/zetcode/dot.png").getImage();
+            apple = new ImageIcon("src/zetcode/apple.png").getImage();
+            head = new ImageIcon("src/zetcode/head.png").getImage();
+        } catch (Exception e) {
+            System.out.println("Error loading images: " + e.getMessage());
+        }
+    }
+
+    private void initGame() {
+        dots = 3;
+        score = 0;
+
+        for (int z = 0; z < dots; z++) {
+            x[z] = 50 - z * DOT_SIZE;
+            y[z] = 50;
+        }
+
+        locateApple();
+        timer = new Timer(DELAY, this);
+        timer.start();
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        doDrawing(g);
+    }
+
+    private void doDrawing(Graphics g) {
+        if (inGame) {
+            g.drawImage(apple, apple_x, apple_y, this);
+
+            for (int z = 0; z < dots; z++) {
+                if (z == 0) {
+                    g.drawImage(head, x[z], y[z], this);
+                } else {
+                    g.drawImage(ball, x[z], y[z], this);
+                }
+            }
+
+            // Draw score
+            g.setColor(Color.white);
+            g.setFont(new Font("Helvetica", Font.BOLD, 14));
+            g.drawString("Score: " + score, 10, 20);
+
+            Toolkit.getDefaultToolkit().sync();
+        } else {
+            gameOver(g);
+        }
+    }
+
+    private void gameOver(Graphics g) {
+        String msg = "Game Over";
+        String restartMsg = "Press ENTER to restart";
+        Font font = new Font("Helvetica", Font.BOLD, 14);
+        FontMetrics metr = getFontMetrics(font);
+
+        g.setColor(Color.white);
+        g.setFont(font);
+        g.drawString(msg, (B_WIDTH - metr.stringWidth(msg)) / 2, B_HEIGHT / 2 - 10);
+        g.drawString(restartMsg, (B_WIDTH - metr.stringWidth(restartMsg)) / 2, B_HEIGHT / 2 + 10);
+    }
+
+    private void checkApple() {
+        if ((x[0] == apple_x) && (y[0] == apple_y)) {
+            dots++;
+            score += 10;
+            locateApple();
+        }
+    }
+
+    private void move() {
+        for (int z = dots; z > 0; z--) {
+            x[z] = x[z - 1];
+            y[z] = y[z - 1];
+        }
+
+        if (leftDirection) x[0] -= DOT_SIZE;
+        if (rightDirection) x[0] += DOT_SIZE;
+        if (upDirection) y[0] -= DOT_SIZE;
+        if (downDirection) y[0] += DOT_SIZE;
+    }
+
+    private void checkCollision() {
+        for (int z = dots; z > 0; z--) {
+            if ((z > 4) && (x[0] == x[z]) && (y[0] == y[z])) {
+                inGame = false;
+            }
+        }
+
+        if (x[0] < 0 || x[0] >= B_WIDTH || y[0] < 0 || y[0] >= B_HEIGHT) {
+            inGame = false;
+        }
+
+        if (!inGame) {
+            timer.stop();
+        }
+    }
+
+    private void locateApple() {
+        int r = (int) (Math.random() * RAND_POS);
+        apple_x = r * DOT_SIZE;
+
+        r = (int) (Math.random() * RAND_POS);
+        apple_y = r * DOT_SIZE;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (inGame) {
+            checkApple();
+            checkCollision();
+            move();
+        }
+        repaint();
+    }
+
+    private class TAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int key = e.getKeyCode();
+
+            if ((key == KeyEvent.VK_LEFT) && (!rightDirection)) {
+                leftDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_RIGHT) && (!leftDirection)) {
+                rightDirection = true;
+                upDirection = false;
+                downDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_UP) && (!downDirection)) {
+                upDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            if ((key == KeyEvent.VK_DOWN) && (!upDirection)) {
+                downDirection = true;
+                rightDirection = false;
+                leftDirection = false;
+            }
+
+            // Restart game on ENTER
+            if (!inGame && key == KeyEvent.VK_ENTER) {
+                inGame = true;
+                initGame();
+                repaint();
+            }
+        }
+    }
+}
