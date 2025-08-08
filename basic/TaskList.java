fd8151e6405debd4cfa500fdcba4c25712d1328f
Tejesh00318 <tejeshg92@gmail.com>
2025-08-08 11:56:05 +0530

    Update TaskList.java

--- basic/TaskList.java
+++ basic/TaskList.java
@@ -2,7 +2,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class TaskListApp {
+public class TaskList {
     public static void main(String[] args) {
         TaskList taskList = new TaskList();
 
